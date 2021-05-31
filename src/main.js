41968c7523096abd0d7487d238b95492b87e662c
Thiago Bezerra <thiago.bezerra@slu.df.gov.br>
2021-05-31 17:11:11 -0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 
-createApp(App).use(store).use(router).mount("#app");
+library.add(faUserSecret)
+
+let app = createApp(App)
+app.component("font-awesome-icon", FontAwesomeIcon)
+app.use(store)
+app.use(router)
+app.mount("#app");
