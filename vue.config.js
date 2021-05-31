74f76e4bf71aa064455527c84064b1d2f50a4f0f
Thiago Bezerra <thiago.bezerra@slu.df.gov.br>
2021-05-31 17:11:11 -0300

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    lintOnSave: false,
+
+    css: {
+        loaderOptions: {
+            sass: {
+                prependData: `
+                      @import "@/assets/scss/styles.scss";
+                  `
+            }
+        }
+    },
+}
